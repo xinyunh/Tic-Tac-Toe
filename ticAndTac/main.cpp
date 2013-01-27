61c799a0e47e08e7ca072e2fcb98d6fc894f1f77
Xinyun <xinyun@Xinyuns-MacBook-Pro.local>
2013-01-26 22:07:07 -0500

    Initial Commit

---
+++ ticAndTac/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ticAndTac
+//
+//  Created by Xinyun on 1/26/13.
+//  Copyright (c) 2013 Xinyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
